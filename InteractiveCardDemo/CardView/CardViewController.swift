9ab311924b4e34c0e896934e4823a5bbfaec4c1d
zjfsharp <zjfsharp@sina.cn>
2020-07-29 22:27:56 +0800

    add CardViewController and nib

---
+++ InteractiveCardDemo/CardView/CardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CardViewController.swift
+//  InteractiveCardDemo
+//
+//  Created by zjfsharp on 2020/7/29.
+//  Copyright © 2020 zjfsharp. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    @IBOutlet weak var handleArea: UIView!
+    
+    
+}
--- InteractiveCardDemo/ViewController.swift
+++ InteractiveCardDemo/ViewController.swift
@@ -9,10 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum CardState {
+        case expand
+        case collapsed
+    }
+    
+    var cardViewController: CardViewController!
+    var visualEffectView: UIVisualEffectView!
+    
+    let cardViewheight: CGFloat = 600
+    let cardHandleAreaHeight: CGFloat = 65
+    
+    var cardVisible = false
+    var nextState:CardState {
+        return cardVisible ? .collapsed : .expand
+    }
+    
+    var runningAnimators = [UIViewPropertyAnimator]()
+    var animatorProgressWhenInterupted: CGFloat = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .link
+        setCardView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+    }
+    
+    func setCardView() {
+        visualEffectView = UIVisualEffectView()
+        visualEffectView.frame = self.view.frame
+        self.view.addSubview(visualEffectView)
+        
+        cardViewController = CardViewController(nibName: "CardViewController", bundle: nil)
+        addChild(cardViewController)
+        self.view.addSubview(cardViewController.view)
+        
+        cardViewController.view.frame = CGRect(x: 0, y: self.view.frame.height - cardHandleAreaHeight, width: self.view.bounds.width, height: cardViewheight)
+        cardViewController.view.clipsToBounds = true
+        
     }
 
 
